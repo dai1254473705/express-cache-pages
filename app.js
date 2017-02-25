5e5fbb73c1142cae1f8bf4ddf8d6568ff756451a
dai1254473705 <1254473705@qq.com>
2017-02-25 22:57:12 +0800

    2017.2.25 22:57
    Signed-off-by: dai1254473705 <1254473705@qq.com>

--- app.js
+++ app.js
@@ -2,23 +2,29 @@ var express = require("express");
 var morgan  = require("morgan");
 var path 	= require('path');
 var app     = express();
+var favicon = require('serve-favicon');
 var ejs 	= require('ejs');
 
 //app.get("/",function(req,res){
 //	res.send("hello world");
 //});
 
+var index = require('./routes/index');
+
 // view engine setup
 app.engine('html', ejs.__express);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
 
-app.use(morgan("dev"));
 
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, 'public')));
 //主页
 app.use('/', index);
-
-var server = app.listen(3000,function(){
-	console.log("http://127.0.0.1:3000");
-});
+app.use(function(req, res) {
+//	console.log(req.res)
+	res.end(req.url)
+})
+var server = app.listen(9010);
 module.exports = app;
